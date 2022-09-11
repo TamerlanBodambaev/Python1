9a6a8b4df97cc22298ef20743e72d8f0a1880b34
TamerlanBodambaev <96405697+TamerlanBodambaev@users.noreply.github.com>
2022-09-11 21:31:25 +0600

    Add files via upload

---
+++ savepra1.py
@@ -0,0 +1,7 @@
+a = input('Your Last name,first name?')
+b = input('How old are you?')
+c = input('Where do you live?')
+print('Your Last name,first name',a)
+print('Your age',b)
+print('You live in',c)
+        
